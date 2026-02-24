3f67f1abff42746a66dfe8fe73c75affc1efdcd1
Raju Patel <rajupatelbasnijutha@gmail.com>
2026-02-25 01:09:20 +0530

    Update media and posts destination URLs in configurl fix next navigation

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,18 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: "/media/:path*",
+//         destination: "http://127.0.0.1:8000/media/:path*",
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -5,7 +20,11 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/media/:path*",
-        destination: "http://127.0.0.1:8000/media/:path*",
+        destination: "https://indiajobs-2.onrender.com/media/:path*",
+      },
+      {
+        source: "/posts/:path*",
+        destination: "https://indiajobs-2.onrender.com/posts/:path*",
       },
     ];
   },
