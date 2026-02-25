4d4ed9f5cc0b35cb504a447b598972ca9705806a
Raju Patel <rajupatelbasnijutha@gmail.com>
2026-02-25 11:32:57 +0530

    Add sitemap function for job URLs

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://indiajobs-2.onrender.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://indiajobs-2.onrender.com/jobs",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://indiajobs-2.onrender.com/jobs/govt",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://indiajobs-2.onrender.com/jobs/private",
+      lastModified: new Date(),
+    },
+  ];
+}
