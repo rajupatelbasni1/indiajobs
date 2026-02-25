d32a6fd24ceca4da52ad32608a397a145bd5859d
RAJU PATEL <rajupatel@RAJUs-MacBook-Air.local>
2026-02-26 01:28:50 +0530

    Added sitemap  pages

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -14,6 +14,11 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const staticPages: MetadataRoute.Sitemap = [
     "",
     "/jobs",
+    "/about",
+    "/contact",
+    "/disclaimer",
+    "/dmca",
+    "/privacy-policy",
     "/jobs/govt",
     "/jobs/private",
     "/jobs/upcoming",
